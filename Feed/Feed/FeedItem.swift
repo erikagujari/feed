9899dc73bd85a63099c24510b23ffcf83acbeb46
Erik Agujari <erik.agujari@soprasteria.com>
2020-05-27 13:36:06 +0200

    add project and FeedItem

---
+++ Feed/Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Erik Agujari on 27/05/2020.
+//  Copyright © 2020 Erik Agujari. All rights reserved.
+//
+import Foundation
+
+struct FeedItem {
+    let uuid: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: String
+}
