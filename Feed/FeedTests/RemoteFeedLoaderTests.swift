4a0fd7d43a9092554d294fc0d46b063af2b4afc2
Erik Agujari <erik.agujari@soprasteria.com>
2020-05-29 08:32:07 +0200

    RemoteFeedLoader does not create data upon creation

---
+++ Feed/FeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  FeedTests
+//
+//  Created by Erik Agujari on 29/05/2020.
+//  Copyright © 2020 Erik Agujari. All rights reserved.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        XCTAssertNil(client.requestedURL)
+    }
+}
