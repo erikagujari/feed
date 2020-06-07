bc3d4978539686b9e277f4e8210fd4089af0e11b
Erik Agujari <erik.agujari@soprasteria.com>
2020-06-07 12:45:04 +0200

    Add end to end tests in a separate test target

--- Feed/Feed/Feed Feature/FeedItem.swift
+++ Feed/Feed/Feed Feature/FeedItem.swift
@@ -12,4 +12,11 @@ public struct FeedItem: Equatable {
     public let description: String?
     public let location: String?
     public let imageUrl: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageUrl: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageUrl = imageUrl
+    }
 }
