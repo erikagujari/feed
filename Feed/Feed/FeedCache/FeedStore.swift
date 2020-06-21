bd06f9cd5d52f021caaf479c08c6a0e53e1477aa
Erik Agujari <erik.agujari@soprasteria.com>
2020-06-21 12:21:34 +0200

    move `FeedStore` to its own file

---
+++ Feed/Feed/FeedCache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  Feed
+//
+//  Created by Erik Agujari on 21/06/2020.
+//  Copyright © 2020 Erik Agujari. All rights reserved.
+//
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = ((Error?) -> Void)
+    typealias InsertionCompletion = ((Error?) -> Void)
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
