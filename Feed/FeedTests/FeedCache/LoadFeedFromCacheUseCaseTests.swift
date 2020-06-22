af25b8bfc8461c8402357dc0bd9cd0cad039c036
Erik Agujari <erik.agujari@soprasteria.com>
2020-06-22 10:30:29 +0200

    Load command cache retrieval

--- Feed/FeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ Feed/FeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -14,6 +14,12 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let (_, store) = makeSUT()
         XCTAssertEqual(store.receivedMessages, [])
     }
+    
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        sut.load()
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 }
 
 extension LoadFeedFromCacheUseCaseTests {
