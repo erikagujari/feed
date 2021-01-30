3ba0ef03bd834eab6bf3787c3662e7baee618eff
Erik Agujari <erikagujari@gmail.com>
2021-01-30 12:17:44 +0100

    Move `FeedViewController` into production code

---
+++ Feed/FeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  FeediOS
+//
+//  Created by Erik Agujari on 30/1/21.
+//  Copyright © 2021 Erik Agujari. All rights reserved.
+//
+import Feed
+import UIKit
+
+final public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load(completion: { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        })
+    }
+}
