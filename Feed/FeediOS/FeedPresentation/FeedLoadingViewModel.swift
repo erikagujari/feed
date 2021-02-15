d617278fe33507b372394030ee0634012f016e75
Erik Agujari <erikagujari@gmail.com>
2021-02-15 09:04:56 +0100

    Move `ViewModels` to separate files

---
+++ Feed/FeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  FeediOS
+//
+//  Created by Erik Agujari on 15/2/21.
+//  Copyright © 2021 Erik Agujari. All rights reserved.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ Feed/FeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  FeediOS
+//
+//  Created by Erik Agujari on 15/2/21.
+//  Copyright © 2021 Erik Agujari. All rights reserved.
+//
+import Feed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
