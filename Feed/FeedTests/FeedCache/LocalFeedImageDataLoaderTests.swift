eb065915241538df3588bd7ca46597d02075dd5c
Erik Agujari <erikagujari@gmail.com>
2021-02-26 15:35:28 +0100

    `LocalFeedImageDataLoader.loadImageData` delivers not found error when store can't find image data for url

--- Feed/FeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
+++ Feed/FeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
@@ -20,6 +20,7 @@ final class LocalFeedImageDataLoader {
     }
     public enum Error: Swift.Error {
         case failed
+        case notFound
     }
     
     private let store: FeedImageDataStore
@@ -30,7 +31,9 @@ final class LocalFeedImageDataLoader {
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         store.retrieve(dataForURL: url) { result in
-            completion(.failure(Error.failed))
+            completion(result
+                        .mapError { _ in Error.failed }
+                        .flatMap { _ in .failure(Error.notFound)})
         }
         return Task()
     }
@@ -46,21 +49,29 @@ final class LocalFeedImageDataLoaderTests: XCTestCase {
     func test_loadImageDataFromURL_requestsStoredDataForURL() {
         let (sut, store) = makeSUT()
         let url = anyURL()
-
+        
         _ = sut.loadImageData(from: url) { _ in }
-
+        
         XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
     }
     
     func test_loadImageDataFromURL_failsOnStoreError() {
-            let (sut, store) = makeSUT()
-
-            expect(sut, toCompleteWith: failed(), when: {
-                let retrievalError = anyNSError()
-                store.complete(with: retrievalError)
-            })
-        }
+        let (sut, store) = makeSUT()
         
+        expect(sut, toCompleteWith: failed(), when: {
+            let retrievalError = anyNSError()
+            store.complete(with: retrievalError)
+        })
+    }
+    
+    func test_loadImageDataFromURL_deliversNotFoundErrorOnNotFound() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: notFound(), when: {
+            store.complete(with: .none)
+        })
+    }
+    
     //MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
         let store = StoreSpy()
@@ -86,31 +97,39 @@ final class LocalFeedImageDataLoaderTests: XCTestCase {
         func complete(with error: Error, at index: Int = 0) {
             completions[index](.failure(error))
         }
+        
+        func complete(with data: Data?, at index: Int = 0) {
+            completions[index](.success(data))
+        }
     }
     
     private func failed() -> FeedImageDataLoader.Result {
         return .failure(LocalFeedImageDataLoader.Error.failed)
     }
     
+    private func notFound() -> FeedImageDataLoader.Result {
+        return .failure(LocalFeedImageDataLoader.Error.notFound)
+    }
+    
     private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
-
+        
         _ = sut.loadImageData(from: anyURL()) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-
+                
             case (.failure(let receivedError as LocalFeedImageDataLoader.Error),
                   .failure(let expectedError as LocalFeedImageDataLoader.Error)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-
+                
             default:
                 XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
-
+            
             exp.fulfill()
         }
-
+        
         action()
         wait(for: [exp], timeout: 1.0)
     }
