f8773e6f9c9a981d16ed6f762e7b2de0effcbced
Erik Agujari <erikagujari@gmail.com>
2021-04-01 16:13:40 +0200

    Extract `uniqueFeed` factory helper into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/XCTestCase+Helpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/XCTestCase+Helpers.swift
@@ -5,10 +5,13 @@
 //  Created by Erik Agujari on 31/3/21.
 //
 
-import XCTest
+import Feed
+import Foundation
 
-extension XCTestCase {
-    func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0, userInfo: nil)
-    }
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0, userInfo: nil)
+}
+
+func uniqueFeed() -> [FeedImage] {
+    return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
 }
