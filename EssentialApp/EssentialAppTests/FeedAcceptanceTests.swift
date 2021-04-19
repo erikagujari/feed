f38a126910216d498c5b0c08125a8d87273d27f3
Erik Agujari <erikagujari@gmail.com>
2021-04-19 10:04:00 +0200

    displays empty feed on launch when customer has no connectivity and no cache

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -31,14 +31,15 @@ final class FeedAcceptanceTests: XCTestCase {
     }
     
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+        let feed = launch(httpClient: .offline, store: .empty)
+        
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 0)
     }
 }
 
 private extension FeedAcceptanceTests {
     func launch(httpClient: HTTPClientStub = .offline,
                 store: InMemoryFeedStore = .empty) -> FeedViewController {
-        let store = InMemoryFeedStore.empty
-        let httpClient = HTTPClientStub.online(response)
         let sut = SceneDelegate(httpClient: httpClient, store: store)
         sut.window = UIWindow()
         sut.configureWindow()
